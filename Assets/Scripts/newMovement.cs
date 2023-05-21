7b4f482e92e7452649f8b71de673343729a71ce4
AyoTheNinth <103271572+AyoTheNinth@users.noreply.github.com>
2023-05-21 22:01:36 +0300

    очень много всего. Система стамины
    система стамины, ui и много всякой всячины

--- Assets/Scripts/newMovement.cs
+++ Assets/Scripts/newMovement.cs
@@ -2,21 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMove : MonoBehaviour
+public class newMovement : MonoBehaviour
 {
     [SerializeField] private LayerMask groundmask;
     private Rigidbody2D rb;
     private SpriteRenderer sr;
     private Animator anim;
-    private CapsuleCollider2D bxcoll;
+    private BoxCollider2D capcoll;
     public GameObject AltPlayer;
     public GameObject Player;
     public GameObject eyes;
     public bool visionOn;
 
-
     [SerializeField] private float moveSpeed = 7f;
     [SerializeField] private float jumpForce = 14f;
+
 
     private float movX = 0f;
     // Start is called before the first frame update
@@ -25,7 +25,7 @@ public class PlayerMove : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
         sr = GetComponent<SpriteRenderer>();
-        bxcoll = GetComponent<CapsuleCollider2D>();
+        capcoll = GetComponent<BoxCollider2D>();
 
 
 
@@ -33,38 +33,45 @@ public class PlayerMove : MonoBehaviour
 
 
     // Update is called once per frame
-    private void Update()
+    private void FixedUpdate()
     {
 
+        Movement();
+        //rb.velocity = new Vector2(movX * moveSpeed, rb.velocity.y);
 
-        movement();
-        jumping();
+        if (isGrounded() && Input.GetButtonDown("Jump"))
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        }
+
+
+
+
         //AllignSprites();
         AnimUpdate();
 
     }
 
-    private void movement()
+    private void Movement()
     {
-        //float movX = Input.GetAxis("Horizontal");
-
-        //float moveVertical = Input.GetAxis("Vertical");
-
-        //Vector3 movement = new Vector3(movX, 0.0f, moveVertical);
-
-        //transform.Translate(movement * moveSpeed * Time.fixedDeltaTime);
-
-
-        movX = Input.GetAxis("Horizontal");
-        rb.velocity = new Vector2(movX * moveSpeed, rb.velocity.y);
+        movX = Input.GetAxisRaw("Horizontal");
+        float movY = Input.GetAxisRaw("Vertical");
+        Vector2 movement = new Vector2(movX, movY);
+        transform.Translate(movement * moveSpeed * Time.fixedDeltaTime);
     }
-    private void jumping()
-    {
-        if (isGrounded() && Input.GetButtonDown("Jump"))
-        {
-            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
-        }
-    }
+
+
+
+    //private void MovementLogic()
+    //{
+    //    float moveHorizontal = Input.GetAxis("Horizontal");
+
+    //    float moveVertical = Input.GetAxis("Vertical");
+
+    //    Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+    //    rb.AddForce(movement * moveSpeed);
+    //}
     private void AnimUpdate()
     {
         if (movX > 0f)
@@ -84,13 +91,22 @@ public class PlayerMove : MonoBehaviour
 
         anim.SetFloat("jumping", rb.velocity.y);
 
-        
+        //if (visionOn == true)
+        //{
+        //    anim.SetBool("eyes", true);
+        //}
+        //else if(visionOn == false)
+        //{
+        //    anim.SetBool("eyes", false);
+        //}
+
     }
+
 
     private bool isGrounded()
     {
-        float extraHeight = 1f;
-        RaycastHit2D raycastHit = Physics2D.Raycast(bxcoll.bounds.center, Vector2.down, bxcoll.bounds.extents.y + extraHeight, groundmask);
+        float extraHeight = 50f;
+        RaycastHit2D raycastHit = Physics2D.Raycast(capcoll.bounds.min, Vector2.down, capcoll.bounds.extents.y + extraHeight, groundmask);
         Color Raycolor;
         if (raycastHit.collider != null)
         {
@@ -100,7 +116,7 @@ public class PlayerMove : MonoBehaviour
         {
             Raycolor = Color.red;
         }
-        Debug.DrawRay(bxcoll.bounds.center, Vector2.down * (bxcoll.bounds.extents.y));
+        Debug.DrawRay(capcoll.bounds.center, Vector2.down * (capcoll.bounds.extents.y));
         return raycastHit.collider != null;
     }
 }
--- Assets/Scripts/vision.cs
+++ Assets/Scripts/vision.cs
@@ -1,36 +1,102 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro.EditorUtilities;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class vision : MonoBehaviour
 {
+
     public GameObject eyes;
     public bool visionOn;
+    bool staminaZero;
+    float totalStamina = 6;
+    float fullStamina = 6;
+
+    [SerializeField] public Slider staminabar;
+
+    public static Action VisionOn_ev;
+    public static Action VisionOff_ev;
 
     // Start is called before the first frame update
     void Start()
     {
+        //staminabar = GameObject.FindGameObjectWithTag("StaminaBar").GetComponent<Slider>();
         eyes = GameObject.FindGameObjectWithTag("Vision");
         eyes.SetActive(false);
         visionOn = false;
-
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButtonDown("Vision") && visionOn == false)
+        Vision();
+        StaminaIsDepleting();
+        StaminaUpdate(totalStamina);
+    }
+    private void Vision()
+    {
+        if (Input.GetButtonDown("Vision") && visionOn == false && staminaZero == false)
         {
-            Debug.Log(KeyCode.Q + "нажата");
-            visionOn = true;
-            eyes.SetActive(true);
+            if( staminaZero != true)
+            {
+                SetVisionOn();
+            }
+            
         }
         else if (Input.GetButtonDown("Vision") && visionOn == true)
         {
-            visionOn = false;
-            eyes.SetActive(false);
+            SetVisionOff();
         }
+    }
+    
+    private void StaminaUpdate(float maxStamina)
+    {
+        staminabar.value = maxStamina;
+    }
 
+    private void SetVisionOn()
+    {
+        Debug.Log(KeyCode.Q + "нажата");
+        visionOn = true;
+        eyes.SetActive(true);
+        VisionOn_ev.Invoke();
         
+    }
+    private void SetVisionOff()
+    {
+        visionOn = false;
+        eyes.SetActive(false);
+        VisionOff_ev.Invoke();
+    }
+
+    private void StaminaIsDepleting()
+    {
+        if (visionOn == true && staminaZero == false)
+        {
+            if (totalStamina > 0f)
+            {
+                
+                totalStamina -= 1 * Time.deltaTime;
+                Debug.Log(totalStamina);
+            }
+            else
+            {
+                totalStamina = 0;
+                staminaZero = true;
+                visionOn = false;
+                SetVisionOff();
+                Debug.Log("Стамина кончилась");
+            }
+        }
+        if (staminaZero == true || (totalStamina < fullStamina && visionOn == false))
+            {
+                totalStamina += 1 * Time.deltaTime;
+                Debug.Log("Стамина перезаряжается");
+            Debug.Log(totalStamina);
+            Debug.Log(staminaZero);
+            staminaZero = false;
+        }
     }
 }
